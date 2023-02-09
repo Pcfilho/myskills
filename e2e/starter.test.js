5d2c52d24d88a52494762f0253f9cb738ec3b070
Paulo <paulofilhobarroso@gmail.com>
2023-02-09 13:55:01 -0300

    Lint + Detox

---
+++ e2e/starter.test.js
@@ -0,0 +1,11 @@
+import {device} from 'detox';
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {});
+
+  it('should test something', async () => {});
+});
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,128 +1,117 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-    View,
-    Text,
-    StyleSheet,
-    TextInput,
-    Platform,
-    FlatList,
-    StatusBar
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+  StatusBar,
 } from 'react-native';
-import { Button } from '../components/Button';
-import { SkillCard } from '../components/SkillCard';
+import {Button} from '../components/Button';
+import {SkillCard} from '../components/SkillCard';
 
 interface SkillData {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 }
 
-export default function Home(){
-    const [newSkill, setNewSkill] = useState('');
-    const [mySkills, setMySkills] = useState<SkillData[]>([]);
-    const [greeting, setGreeting] = useState('');
-    const [hour, setHour] = useState<number>(0);
+export default function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [greeting, setGreeting] = useState('');
+  const [hour, setHour] = useState<number>(0);
 
-
-
-    function handleNewAddSkill() { 
-        if(newSkill.trim()) {
-            const data = {
-                id: String(new Date().getTime()),
-                name: newSkill
-            }
-            setMySkills(oldState => [...oldState, data]);
-            setNewSkill(" ");
-        }
-
+  function handleNewAddSkill() {
+    if (newSkill.trim()) {
+      const data = {
+        id: String(new Date().getTime()),
+        name: newSkill,
+      };
+      setMySkills(oldState => [...oldState, data]);
+      setNewSkill(' ');
     }
+  }
 
-    function handleRemoveSkill(id: string) {
-        setMySkills(oldState => oldState.filter(skill => skill.id !== id));
+  function handleRemoveSkill(id: string) {
+    setMySkills(oldState => oldState.filter(skill => skill.id !== id));
+  }
+
+  function updateGreeting() {
+    const date = new Date().getHours();
+    console.log(date.toString());
+    setHour(date);
+    if (date < 12) {
+      setGreeting('Good morning');
+    } else if (date >= 12 && date < 18) {
+      setGreeting('Good afternoon');
+    } else {
+      setGreeting('Good night');
     }
+  }
 
-    function updateGreeting() {
-        const date = new Date().getHours();
-        console.log(date.toString());
-        setHour(date);
-        if (date < 12) {
-            setGreeting('Good morning');
-        } else if (date >= 12 && date < 18) {
-            setGreeting('Good afternoon');
-        } else {
-            setGreeting('Good night');
-        }
-    }
+  useEffect(() => {
+    const ONE_MINUTE = 60000;
+    setInterval(() => {
+      updateGreeting();
+    }, ONE_MINUTE);
+  }, [hour]);
 
+  useEffect(() => {
+    updateGreeting();
+  }, []);
 
-    useEffect(() => {
-        const ONE_MINUTE = 60000 ;
-        setInterval(() => {
-            updateGreeting();
-        }, ONE_MINUTE);
-    }, [hour]);
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.title}>Welcome, Paulo</Text>
+      <Text style={[styles.title, {fontSize: 12, marginTop: 5}]}>
+        {greeting}
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor="#555"
+        onChangeText={setNewSkill}>
+        {newSkill}
+      </TextInput>
 
-    useEffect(() => {
-        updateGreeting();
-    }, [])
+      <Button onPress={handleNewAddSkill} title="New" />
 
-    return (
-        <View style={styles.container}>
-            <StatusBar barStyle="dark-content" />
-            <Text style={styles.title}>Welcome, Paulo</Text>
-            <Text style={[styles.title, {fontSize: 12, marginTop: 5}]}>
-                {greeting}
-            </Text>
-            <TextInput
-                style={styles.input}
-                placeholder="New Skill"
-                placeholderTextColor="#555"
-                onChangeText={setNewSkill}
+      <Text style={[styles.title, {marginTop: 50}]}>My Skills</Text>
 
-            >
-                {newSkill}
-            </TextInput>
-
-            <Button 
-                onPress={handleNewAddSkill}
-                title="New"
-            />
-            
-            <Text style={[styles.title, { marginTop: 50}]}>
-                My Skills
-            </Text>
-
-            <FlatList
-                data={mySkills}
-                keyExtractor={item => item.id}
-                renderItem={({item}) => (
-                    <SkillCard 
-                        skill={item.name} 
-                        onPress={() => handleRemoveSkill(item.id)}
-                    />
-                )}
-            />
-        </View>
-    )
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <SkillCard
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
+        )}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#121015',
-        paddingHorizontal: 30,
-        paddingVertical: 50,
-    },
-    title: {
-        color: '#fff',
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
-    input: {
-        backgroundColor: '#1F1E25',
-        color: '#fff',
-        fontSize: 18,
-        padding: Platform.OS === 'ios' ? 15 : 10,
-        marginTop: 20,
-        borderRadius: 8
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 20,
+    borderRadius: 8,
+  },
 });
